859fbe790076e551446fb73f23b389f6c51fffa2
Ilyaz22 <so.ilia@yandex.ru>
2023-01-09 15:05:51 +0300

    мелкие доработки

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,9 +12,12 @@ class QuestionsController < ApplicationController
   def create
     question_params = params.require(:question).permit(:body, :user_id)
 
-    @question = Question.create(question_params)
-
-    redirect_to user_path(@question.user), notice: 'Новый вопрос создан!'
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to user_path(@question.user), notice: "Новый вопрос создан!"
+    else
+      redirect_to user_path(params[:question][:user_id]) , alert: "Вопрос пустой или слишком большой"
+    end
   end
 
   def update
