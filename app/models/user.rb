e4da87ccac632e08d5a7675277c01f962bc3095f
Ilyaz22 <so.ilia@yandex.ru>
2023-01-12 21:16:46 +0300

    minor improvements in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ApplicationRecord
+  include Gravtastic
+  gravtastic(secure: true, filetype: :png, size: 100, default: 'retro')
+
   has_secure_password
 
   has_many :questions, dependent: :delete_all
 
   before_validation :downcase_nickname
 
-  validates :nickname, format: { with: /\A\w+\z/ }, length: {maximum: 40}, presence: true, uniqueness: true
+  validates :nickname, format: { with: /\A\w+\z/ }, length: { maximum: 40 }, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :header_color, format: { with: /\A#[a-f0-9]{6}\z/i }
-
-  include Gravtastic
-  gravtastic(secure: true, filetype: :png, size: 100, default: 'retro')
+  validates :header_color, format: { with: /\A#[[:xdigit:]]{6}\z/ }
 
   def to_param
     nickname
