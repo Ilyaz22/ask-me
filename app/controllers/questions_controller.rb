24b0a2f79425c62b85e1e272490d4333daed5963
Ilyaz22 <so.ilia@yandex.ru>
2023-05-01 03:46:46 +0300

    off layout for render 404

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -70,6 +70,6 @@ class QuestionsController < ApplicationController
   end
 
   def render_404
-    render file: "#{Rails.root}/public/404.html", status: 404
+    render file: "#{Rails.root}/public/404.html", status: 404, layout: false
   end
 end
