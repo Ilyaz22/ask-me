9c58db07621cd43f28f40968d47cade1d4c6a091
Ilyaz22 <so.ilia@yandex.ru>
2023-05-30 19:34:01 +0300

    fix questions views

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,5 @@
 module QuestionsHelper
-  def render_with_hashtags(body)
-    sanitize(body.gsub(Tag::REGEX) { |word| link_to word, hashtag_path(word.delete("#")), class: "hashtag-link" })
+  def text_with_hashtags(text)
+    simple_format strip_tags(text).gsub(Tag::REGEX) { |word| link_to word, hashtag_path(word.delete("#")), class: "hashtag-link" }
   end
 end
